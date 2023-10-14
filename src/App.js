42a0db7a9b56366df1473ef2bf7a741931ee69d8
Pk22012003 <punyakakkar34@gmail.com>
2023-10-14 12:57:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  // the text entered by the user
+  const [text, setText] = useState('');
+
+  // word count
+  const [wordCount, setWordCount] = useState(0);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    // array of words
+    const words = text.split(' ');
+
+    // update word count
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+  }, [text]);
+
+  return (
+    <div className='container'>
+      <center>
+      <h2>Responsive Paragraph Word <br/> </h2>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='Hi all'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>
+      </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
